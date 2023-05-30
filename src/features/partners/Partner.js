f4dcca7b8e861e5b09e7c9ef8c9f0fdb6846d1b3
Jarrett Roster <jarrettroster@protonmail.com>
2023-05-29 22:26:52 -0600

    first commit

---
+++ src/features/partners/Partner.js
@@ -0,0 +1,20 @@
+const Partner = ({partner}) => {
+    if (partner) {
+        return (
+            <>
+            <img
+            src={partner.image}
+            alt={partner.name}
+            style={{width:'150px'}}
+            />
+            <div className='m-4'>
+                <h5 className='fw-bold'>{partner.name}</h5>
+                {partner.description}
+            </div>
+            </>
+        )
+    }
+    return null;
+}
+
+export default Partner;
